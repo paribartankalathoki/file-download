c0df8ee5851c74621872ee70e1f2fb57143447e0
paribartan <paribartankalathoki@gmail.com>
2021-08-05 15:07:18 +0545

    added sevice to call the api

---
+++ src/app/main.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from './../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+  baseAPI = environment.apiUrl;
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getEncodedResponseForDownload(cid: string, signInTime: string, leaveingTime: string, uid: string, accessToken: string, idToken: string) {
+    // set params as per your requirement
+    let params = new HttpParams();
+    params = params.append('customerId', cid);
+    params = params.append('signInDateTimeEnd', signInTime);
+    params = params.append('signInDateTimeStart', leaveingTime);
+    params = params.append('userId', uid);
+
+    // set http headers as per needed
+    let headers = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json');
+    headers = headers.append('Accept', 'application/json');
+
+    /**
+     * Note: you can set header using interceptor, here i added manually
+     */
+
+    headers = headers.append('accessToken', accessToken);
+    headers = headers.append('idToken', idToken);
+    headers = headers.append('customerId', cid);
+    headers = headers.append('userId', uid);
+
+    return this.httpClient.get(`${this.baseAPI}`, { headers, responseType: 'blob', observe: 'response' });
+
+  }
+}
